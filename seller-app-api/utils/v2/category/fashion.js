140615ecf874bb882d975c52b48aac117002551e
akshay <akshay@wemotiveforge.com>
2023-09-03 17:15:45 +0530

    fix: attribute key

--- seller-app-api/utils/v2/category/fashion.js
+++ seller-app-api/utils/v2/category/fashion.js
@@ -342,7 +342,7 @@ function itemSchema(items) {
                 ]
             },
             {
-                "code": "attributes",
+                "code": "attribute",
                 "list": attributes
             }
         ]
